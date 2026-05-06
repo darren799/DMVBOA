9ba052811d2943cca38556fa30b4a0e734c15f9a
darren799 <darren@dmvboa.org>
2026-05-06 17:01:26 -0400

    Add navigation toggle functionality

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,6 @@
+const toggle = document.querySelector(".nav-toggle");
+const nav = document.querySelector("#site-nav");
 
+toggle?.addEventListener("click", () => {
+  nav.classList.toggle("open");
+});
